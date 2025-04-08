4a6ab7567bd7c578c5e4dcf77ff67d7b4a4b3f2d
okunade olaitan <olaokunade@gmail.com>
2025-04-08 20:07:29 +0000

    first commit

--- src/Pages/home/index.jsx
+++ src/Pages/home/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar2 } from "./components/Navbar2";
 import { Header30 } from "./components/Header30";
 import { Layout239 } from "./components/Layout239";
-// import { Testimonial18 } from "./components/Testimonial18";
+import { Testimonial18 } from "./components/Testimonial18";
 import { Contact20 } from "./components/Contact20";
 import { Footer3 } from "./components/Footer3";
 
@@ -12,7 +12,7 @@ export default function Home() {
       <Navbar2 />
       <Header30 />
       <Layout239 />
-      {/* <Testimonial18 /> */}
+      <Testimonial18 />
       <Contact20 />
       <Footer3 />
     </div>
